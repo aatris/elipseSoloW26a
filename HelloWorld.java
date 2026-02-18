c9d6df8c1397bccc0e02d078a0d4178c9bb748d6
Ahmad Atris <YOUR_GITHUB_EMAIL>
2026-02-18 13:26:58 -0500

    programmer1 edited same line

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 System.out.println("Programmer 1 new change!!!!!");
-System.out.println("programmertwo did this- programmer1 did thi s"); 
+System.out.println("programmertwo did this- programmer1 did this"); 
 	}
 
 }
